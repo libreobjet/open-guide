af27a99a3c7419b502f453e66b353fccc76b3b41
Julien Deswaef <juego@requiem4tv.com>
2015-12-05 06:33:07 -0500

    Script to fetch pads

---
+++ fetch.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+wget -q https://mathieugabiot.titanpad.com/ep/pad/export/30/latest?format=txt -O definitions.md
+wget -q https://mathieugabiot.titanpad.com/ep/pad/export/31/latest?format=txt -O licences.md
+wget -q https://mathieugabiot.titanpad.com/ep/pad/export/32/latest?format=txt -O motivation.md
+wget -q https://mathieugabiot.titanpad.com/ep/pad/export/33/latest?format=txt -O distribution.md
+wget -q https://mathieugabiot.titanpad.com/ep/pad/export/34/latest?format=txt -O economics.md
+wget -q https://mathieugabiot.titanpad.com/ep/pad/export/35/latest?format=txt -O designing.md
+wget -q https://mathieugabiot.titanpad.com/ep/pad/export/39/latest?format=txt -O links.md
+
+echo "Done."
