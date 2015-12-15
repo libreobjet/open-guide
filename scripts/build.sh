0c8e4bd3312773c86db82e49cb8e0fbeb1084a57
Julien Deswaef <juego@requiem4tv.com>
2015-12-15 05:46:27 -0500

    Cover related files

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,5 +7,5 @@ echo "Creating PDF from Markdown files"
 gitbook pdf ./ ./$OUTPUT_PDF
 echo "Converting PDF"
 gs -o book-outlines.pdf -dNoOutputFonts -sDEVICE=pdfwrite $OUTPUT_PDF
-podofoimpose book-outlines.pdf $OUTPUT_PDF_DIY Booklet-LO.plan
+podofoimpose book-outlines.pdf $OUTPUT_PDF_DIY impositions/Booklet-LO.plan
 echo "Done"
