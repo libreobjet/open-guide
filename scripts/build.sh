69a3909ea1ee5d78a678d5cc38af4a35ae36fc16
Julien Deswaef <juego@requiem4tv.com>
2015-12-16 13:20:41 -0500

    adding Epub and Mobi generation

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+# Run this from the root of the project
+
+EXPORT_FOLDER="exports"
 OUTPUT_PDF="open-guide.pdf"
 OUTPUT_PDF_DIY="open-guide_DIY.pdf"
 
+echo "Creating EPUB from Markdown files"
+gitbook epub ./ ./$EXPORT_FOLDER/open-guide.epub
+
+echo "Creating MOBI from Markdown files"
+gitbook epub ./ ./$EXPORT_FOLDER/open-guide.mobi
+
 echo "Creating PDF from Markdown files"
-gitbook pdf ./ ./$OUTPUT_PDF
+gitbook pdf ./ ./$EXPORT_FOLDER/$OUTPUT_PDF
 echo "Converting PDF"
-gs -o book-outlines.pdf -dNoOutputFonts -sDEVICE=pdfwrite $OUTPUT_PDF
-podofoimpose book-outlines.pdf $OUTPUT_PDF_DIY impositions/Booklet-LO.plan
+gs -o ./$EXPORT_FOLDER/book-outlines.pdf -dNoOutputFonts -sDEVICE=pdfwrite ./$EXPORT_FOLDER/$OUTPUT_PDF
+podofoimpose ./$EXPORT_FOLDER/book-outlines.pdf ./$EXPORT_FOLDER/$OUTPUT_PDF_DIY impositions/Booklet-LO.plan
 echo "Done"
