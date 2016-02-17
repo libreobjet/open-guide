71dea90f4a4f95a28967157d5830ec697c87c5d9
Julien Deswaef <juego@requiem4tv.com>
2016-02-16 22:20:40 -0500

    An image-magick scipt to colorize images

---
+++ scripts/color-image.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Colorize images
+
+COLOR="#247940"
+INPUT=$1
+OUTPUT=$2
+
+convert $INPUT  \
+        -adaptive-resize 1920x1920 \
+        -blur 1x1 -unsharp 0x2.5 \
+        -set colorspace sRGB -separate -seed 1000 -attenuate .8 +noise Multiplicative -combine \
+        -colorspace HSL -channel L -separate \
+        -level 5% \
+        \( \
+           -clone 0 \
+           -fill $COLOR -colorize 100 \
+        \) \
+        \( \
+           -clone 0 \
+           -fill "#FFFFFF" -colorize 100 \
+        \) \
+        \( \
+           -clone 1,2,0 -composite \
+        \) \
+         -delete 0,1,2 \
+      $OUTPUT
